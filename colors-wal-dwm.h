910c9d1942102a5dfa3f3fcd5f6532d2f37ae8b9
nicknase27 <nicknasekatschank@gmail.com>
2024-03-26 17:52:30 +0100

    updating for Gentoo

--- colors-wal-dwm.h
+++ colors-wal-dwm.h
@@ -1,18 +1,18 @@
-static const char norm_fg[] = "#F5F5F5";
-static const char norm_bg[] = "#151515";
-static const char norm_border[] = "#505050";
+static const char norm_fg[] = "#ddd7d6";
+static const char norm_bg[] = "#0d0d12";
+static const char norm_border[] = "#9a9695";
 
-static const char sel_fg[] = "#F5F5F5";
-static const char sel_bg[] = "#808080";
-static const char sel_border[] = "#F5F5F5";
+static const char sel_fg[] = "#ddd7d6";
+static const char sel_bg[] = "#74718B";
+static const char sel_border[] = "#ddd7d6";
 
-//static const char urg_fg[] = "#F5F5F5";
-//static const char urg_bg[] = "#AC4142";
-//static const char urg_border[] = "#AC4142";
+// static const char urg_fg[] = "#ddd7d6";
+// static const char urg_bg[] = "#655D82";
+// static const char urg_border[] = "#655D82";
 
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
     [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
     [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
-    //[SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
+    // [SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
 };
--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMonoNerdFont:size=12" };
+static const char *fonts[]          = { "JetBrainsMonoNerdFont:size=13" };
 static const char dmenufont[]       = "JetBrainsMonoNerdFont:size=12";
 static const char col_gray1[]       = "#e5e9f0";
 static const char col_gray2[]       = "@839496";
@@ -15,8 +15,9 @@ static const char col_gray3[]       = "#434c5e";
 static const char col_gray4[]       = "#3b4252";
 static const char col_cyan[]        = "#d8dee9";
 
-#define BROWSER "brave"
+#define BROWSER "firefox-bin"
 #include "colors-wal-dwm.h"
+#include <X11/XF86keysym.h>
 //static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 //	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -24,7 +25,7 @@ static const char col_cyan[]        = "#d8dee9";
 //};
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -36,6 +37,7 @@ static const Rule rules[] = {
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
   { "mpv",       NULL,     NULL,           0,         0,          0,          0,          -1 },
+  { "discord",       NULL,     NULL,           1 << 4,         0,          0,          0,          -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
@@ -71,6 +73,11 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *flamcmd[] = { "flameshot", "gui", NULL };
+static const char *hibernatecmd[] = { "loginctl","hibernate", NULL };
+
+static const char *medplaypausecmd[] = { "playerctl", "play-pause", NULL };
+static const char *mednextcmd[] = { "playerctl", "next", NULL };
+static const char *medprevcmd[] = { "playerctl", "previous", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -105,6 +112,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_minus,   setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,   setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,   setgaps,        {.i = 0  } },
+  { 0, XF86XK_AudioPlay, spawn, {.v = medplaypausecmd } },
+  { 0, XF86XK_AudioNext, spawn, {.v = mednextcmd } },
+  { 0, XF86XK_AudioPrev, spawn, {.v = medprevcmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -114,6 +124,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+  { MODKEY|ControlMask|ShiftMask, XK_e,   	  spawn,		      {.v = hibernatecmd } },
 	{ MODKEY|ShiftMask,             XK_e,       quit,           {0} },
 };
 
